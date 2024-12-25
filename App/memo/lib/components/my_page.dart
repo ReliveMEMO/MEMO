3134427d582dedf9ade95d0eb0996c3e46c77c7b
Sandinu Pinnawala <74131240+Sandinu@users.noreply.github.com>
2024-12-25 13:24:13 +0530

    Added the bottom Navigation

---
+++ App/memo/lib/components/my_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:memo/pages/create_profile.dart';
+import 'package:memo/pages/profile_page.dart';
+import 'package:solar_icons/solar_icons.dart';
+
+class myPage extends StatefulWidget {
+  const myPage({super.key});
+
+  @override
+  State<myPage> createState() => _myPageState();
+}
+
+class _myPageState extends State<myPage> {
+  final Color colorDark = const Color(0xFF7f31c6);
+
+  List pages = [
+    ProfilePage(),
+    CreateProfile(),
+    ProfilePage(),
+    ProfilePage(),
+    ProfilePage(),
+  ];
+
+  int currentIndex = 0;
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: BottomNavigationBar(
+            showSelectedLabels: false,
+            type: BottomNavigationBarType.fixed,
+            showUnselectedLabels: false,
+            selectedItemColor: colorDark,
+            unselectedItemColor: Colors.grey,
+            elevation: 0,
+            currentIndex: currentIndex,
+            enableFeedback: false,
+            onTap: onTap,
+            backgroundColor: Colors.transparent,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(SolarIconsOutline.home),
+                  label: 'Home',
+                  activeIcon: Icon(SolarIconsBold.home)),
+              BottomNavigationBarItem(
+                  icon: Icon(HugeIcons.strokeRoundedSearch01), label: 'Home'),
+              BottomNavigationBarItem(
+                icon: Icon(SolarIconsOutline.addSquare),
+                label: 'Home',
+                activeIcon: Icon(
+                  SolarIconsBold.addSquare,
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(SolarIconsOutline.bell),
+                label: 'Home',
+                activeIcon: Icon(
+                  SolarIconsBold.bell,
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(SolarIconsOutline.chatRoundCall),
+                label: 'Home',
+                activeIcon: Icon(
+                  SolarIconsBold.chatRoundLine,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

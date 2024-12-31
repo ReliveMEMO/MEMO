0717f3b60873db9a4acfa3d7958029ee70aca60e
Sandinu Pinnawala <74131240+Sandinu@users.noreply.github.com>
2025-01-01 01:20:58 +0530

    Added time for text msg, and datebreakers

--- App/memo/lib/pages/convo_page.dart
+++ App/memo/lib/pages/convo_page.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:hugeicons/hugeicons.dart';
+import 'package:intl/intl.dart';
 import 'package:memo/services/auth_service.dart';
 import 'package:memo/services/msg_encryption.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -15,7 +16,7 @@ class convoPage extends StatefulWidget {
 class _convoPageState extends State<convoPage> {
   final authService = AuthService();
   final msgEncryption = MsgEncryption();
-  final scrollController = ScrollController();
+  var scrollController;
   final List<Map<String, dynamic>> messages = [];
   final int _batchSize = 20;
   bool _loading = false;
@@ -24,6 +25,8 @@ class _convoPageState extends State<convoPage> {
   @override
   void initState() {
     super.initState();
+    scrollController = ScrollController();
+    scrollController.addListener(_scrollListener);
     _loadMessages();
   }
 
@@ -56,93 +59,197 @@ class _convoPageState extends State<convoPage> {
     }
   }
 
+  void _scrollListener() {
+    if (scrollController.position.atEdge) {
+      if (scrollController.position.pixels == 0) {
+        // At the top
+      } else {
+        // At the bottom
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    scrollController.removeListener(_scrollListener);
+    scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Map arguments = ModalRoute.of(context)?.settings.arguments as Map;
     final String chatId = arguments['chatId'];
     final recieverDetails = arguments['recieverDetails'];
+
     return Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 60,
-          title: Row(
-            children: [
-              CircleAvatar(
-                radius: 20,
-                child: ClipOval(
-                  child: CachedNetworkImage(
-                    imageUrl: recieverDetails['profile_pic'] as String,
-                    width: 40,
-                    height: 40,
-                    fit: BoxFit.cover,
-                  ),
+      appBar: AppBar(
+        toolbarHeight: 60,
+        title: Row(
+          children: [
+            CircleAvatar(
+              radius: 20,
+              child: ClipOval(
+                child: CachedNetworkImage(
+                  imageUrl: recieverDetails['profile_pic'] as String,
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.cover,
                 ),
               ),
-              const SizedBox(width: 15),
-              Text(
-                recieverDetails['full_name'] as String,
-                style: TextStyle(fontSize: 20, letterSpacing: 1),
-              ),
-            ],
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(HugeIcons.strokeRoundedCall02),
-              onPressed: () {
-                // Add your onPressed logic here
-              },
+            ),
+            const SizedBox(width: 15),
+            Text(
+              recieverDetails['full_name'] as String,
+              style: TextStyle(fontSize: 20, letterSpacing: 1),
             ),
           ],
         ),
-        body: Stack(children: [
-          Positioned.fill(
-              child: Image.asset(
-            'assets/images/chat_bg.jpg',
-            fit: BoxFit.cover,
-          )),
-          Column(
+        actions: [
+          IconButton(
+            icon: Icon(HugeIcons.strokeRoundedCall02),
+            onPressed: () {
+              // Add your onPressed logic here
+            },
+          ),
+        ],
+      ),
+      body: Stack(children: [
+        Positioned.fill(
+            child: Image.asset(
+          'assets/images/chat_bg.jpg',
+          fit: BoxFit.cover,
+        )),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Column(
             children: [
               Expanded(
                 child: ListView.builder(
                   controller: scrollController,
-                  reverse: true,
+                  reverse: messages.length > 1,
                   itemCount: messages.length,
                   itemBuilder: (context, index) {
                     final message = messages[index];
                     final isSender =
-                        message['sender_id'] == authService.getCurrentUserID()
-                            ? true
-                            : false;
-                    return Align(
-                      alignment: isSender
-                          ? Alignment.centerRight
-                          : Alignment.centerLeft,
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 5, horizontal: 10),
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: isSender ? Colors.blue[100] : Colors.grey[300],
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: message['imageUrl'] != null
-                            ? CachedNetworkImage(
-                                imageUrl: message['imageUrl'] as String,
-                                placeholder: (context, url) =>
-                                    const CircularProgressIndicator(),
-                                errorWidget: (context, url, error) =>
-                                    const Icon(Icons.error),
-                              )
-                            : Text(
-                                msgEncryption.decrypt(message['message'])!,
-                                style: const TextStyle(fontSize: 16),
+                        message['sender_id'] == authService.getCurrentUserID();
+                    final messageTime = DateTime.parse(message[
+                        'time_stamp']); // Assuming 'timestamp' is in ISO 8601 format
+                    final formattedTime = DateFormat('hh:mm a').format(
+                        messageTime); // Using intl package for formatting
+                    final formattedDate =
+                        DateFormat('dd MMM yyyy').format(messageTime);
+
+                    bool showDateHeader = true;
+                    if (index > 0) {
+                      final previousMessageTime =
+                          DateTime.parse(messages[index - 1]['time_stamp']);
+                      final previousFormattedDate =
+                          DateFormat('dd MMM yyyy').format(previousMessageTime);
+                      showDateHeader = formattedDate != previousFormattedDate;
+                    }
+
+                    return Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          if (showDateHeader)
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10),
+                              child: Center(
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 5, horizontal: 15),
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[200],
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Text(
+                                    formattedDate,
+                                    style: TextStyle(
+                                        fontSize: 12, color: Colors.grey[700]),
+                                  ),
+                                ),
                               ),
-                      ),
-                    );
+                            ),
+                          Align(
+                            alignment: isSender
+                                ? Alignment.centerRight
+                                : Alignment.centerLeft,
+                            child: Container(
+                              margin: const EdgeInsets.symmetric(
+                                  vertical: 5, horizontal: 10),
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 15),
+                              decoration: BoxDecoration(
+                                color: isSender
+                                    ? Colors.blue[100]
+                                    : Colors.grey[300],
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  if (message['imageUrl'] != null)
+                                    CachedNetworkImage(
+                                      imageUrl: message['imageUrl'] as String,
+                                      placeholder: (context, url) =>
+                                          const CircularProgressIndicator(),
+                                      errorWidget: (context, url, error) =>
+                                          const Icon(Icons.error),
+                                    )
+                                  else
+                                    Text(
+                                      msgEncryption
+                                          .decrypt(message['message'])!,
+                                      style: const TextStyle(fontSize: 16),
+                                    ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    formattedTime,
+                                    style: TextStyle(
+                                        fontSize: 12, color: Colors.grey[600]),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ]);
                   },
                 ),
               ),
             ],
-          )
-        ]));
+          ),
+        ),
+      ]),
+      bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.camera_alt),
+                onPressed: () {
+                  // Add logic for camera
+                },
+              ),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Type a message...',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: () {},
+              ),
+            ],
+          )),
+    );
   }
 }

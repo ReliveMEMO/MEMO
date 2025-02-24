bfc2fdb319995c1487e980948bf1861d4840110d
ThisasRanchagoda <ranchagoda.thisas@gmail.com>
2025-02-24 17:31:31 +0530

    search page error

--- App/memo/lib/pages/my_page.dart
+++ App/memo/lib/pages/my_page.dart
@@ -5,7 +5,7 @@ import 'package:memo/pages/create_profile.dart';
 import 'package:memo/pages/create_timeline.dart';
 import 'package:memo/pages/profile_page.dart';
 import 'package:solar_icons/solar_icons.dart';
-
+import 'package:memo/pages/search_page.dart';
 class myPage extends StatefulWidget {
   const myPage({super.key});
 
@@ -18,7 +18,7 @@ class _myPageState extends State<myPage> {
 
   List pages = [
     ProfilePage(),
-    CreateProfile(),
+    SearchPage(),
     ProfilePage(),
     CreateTimeline(),
     ChatPage(),

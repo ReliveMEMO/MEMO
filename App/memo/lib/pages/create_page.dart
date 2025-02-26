ebc643a28b65e087f38cac1adf542dde532c3793
Monali Suriarachchi <monalisuriarachchi@gmail.com>
2025-02-27 00:39:05 +0530

    Upadte create_page Structure

---
+++ App/memo/lib/pages/create_page.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:memo/components/avatar_upload.dart';
+import 'package:memo/components/textField.dart';
+
+class CreatePage extends StatefulWidget {
+  const CreatePage({super.key});
+
+  @override
+  State<CreatePage> createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  // Controllers for text fields
+  final TextEditingController pageNameController = TextEditingController();
+  final TextEditingController yearController = TextEditingController();
+  final TextEditingController aboutUsController = TextEditingController();
+
+  // Styling Colors
+  final Color colorLight = const Color.fromARGB(255, 248, 240, 255);
+  final Color colorDark = const Color(0xFF7f31c6);
+
+  // Image and Error Text Variables
+  File? _imageFile;
+  String? pageNameErrorText;
+  String? yearErrorText;
+  String? aboutUsErrorText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [],
+        ),
+      ),
+    );
+  }
+}

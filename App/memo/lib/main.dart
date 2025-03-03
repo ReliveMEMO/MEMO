51ad603b2c959faba34281819457e03b1948eef0
Monali Suriarachchi <monalisuriarachchi@gmail.com>
2025-03-03 22:54:51 +0530

    Created base structure for settings_page

--- App/memo/lib/main.dart
+++ App/memo/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:memo/pages/NewMemo.dart';
 import 'package:memo/pages/convo_page.dart';
+import 'package:memo/pages/create_page.dart';
 import 'package:memo/pages/create_profile.dart';
 import 'package:memo/pages/create_timeline.dart';
 import 'package:memo/pages/login_page.dart';
@@ -15,9 +17,6 @@ import 'package:memo/services/auth_service.dart';
 import 'package:memo/services/notification.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
-import 'pages/NewMemo.dart';
-import 'pages/create_page.dart';
 
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
@@ -95,6 +94,7 @@ class MyApp extends StatelessWidget {
         '/new-memo': (context) => NewMemo(),
         '/create-timeline': (context) => CreateTimeline(),
         '/create-page': (context) => CreatePage(),
+        '/settings-page': (context) => SettingsPage(),
         //Testing the CI pipeline xoxo
       },
     );

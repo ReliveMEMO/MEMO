be6b9bc272795ce35179f77bca9594d82f6e0fc6
Monali Suriarachchi <monalisuriarachchi@gmail.com>
2025-03-04 00:17:28 +0530

    Update settings_page with account privacy option

--- App/memo/lib/pages/settings_page.dart
+++ App/memo/lib/pages/settings_page.dart
@@ -1,4 +1,6 @@
+// Settings Page
 import 'package:flutter/material.dart';
+import 'package:memo/pages/account_privacy_page.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -6,6 +8,8 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  bool isPrivate = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +18,38 @@ class _SettingsPageState extends State<SettingsPage> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [],
+          children: [
+            ListTile(
+              leading: Icon(Icons.lock, color: Colors.black),
+              title: Text("Account privacy",
+                  style: TextStyle(fontSize: 16, color: Colors.black54)),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(isPrivate ? "Private" : "Public",
+                      style: TextStyle(color: Colors.black)),
+                  SizedBox(width: 8),
+                  Icon(Icons.arrow_forward_ios,
+                      size: 16, color: Colors.black54),
+                ],
+              ),
+              onTap: () async {
+                final result = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        AccountPrivacyPage(initialIsPrivate: isPrivate),
+                  ),
+                );
+                if (result != null) {
+                  setState(() {
+                    isPrivate = result;
+                  });
+                }
+              },
+            ),
+            Divider(),
+          ],
         ),
       ),
     );

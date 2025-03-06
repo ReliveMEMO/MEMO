ffebf7ee2c8a5e89145d4cb85b5801593fd895c5
SadinsaWelagedara <wasadinsa@gmail.com>
2025-03-06 07:54:04 +0530

    Added image box and heading

--- App/memo/lib/pages/memoryReminder_page.dart
+++ App/memo/lib/pages/memoryReminder_page.dart
@@ -7,11 +7,15 @@ class MemoryReminderPopup extends StatelessWidget {
   Widget build(BuildContext context) {
     // Fetch arguments safely
     final arguments = ModalRoute.of(context)?.settings.arguments;
-    final args = arguments is Map<String, dynamic> ? arguments : {};
+    final args = arguments is Map<String, dynamic> ? arguments : {
+      'imageUrl': "https://via.placeholder.com/300"
+    };
     VoidCallback onClose = args['onClose'] ??
         () {
           Navigator.of(context).pop();
         };
+
+    String imageUrl = args['imageUrl'] ?? '';    
 
     return Stack(
       children: [
@@ -38,7 +42,54 @@ class MemoryReminderPopup extends StatelessWidget {
             ),
           ),
         ),
+        
+          // Main Content
+        Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                "#ThrowBacks",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              //image box
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  color: Colors.black,
+                  padding: const EdgeInsets.all(8),
+                  child: Image.network(
+                    imageUrl,
+                    width: 300,
+                    height: 350,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              const Text(
+                "Remember that day",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),  
+        ),  
+
       ],
     );
+    
   }
 }
